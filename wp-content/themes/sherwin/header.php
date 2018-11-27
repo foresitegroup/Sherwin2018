63f558b40fd2f51de87f49a9ae8aeaa4dd8ee7d0
foresitegroup <mark@foresitegrp.com>
2018-11-27 16:33:08 -0600

    Replace logo

--- wp-content/themes/sherwin/header.php
+++ wp-content/themes/sherwin/header.php
@@ -82,7 +82,7 @@
 
 	<header id="main-header"<?php if(is_front_page()) echo ' class="home-header home-header-border'; ?>">
 		<div class="site-width">
-	    <a href="<?php echo home_url(); ?>" id="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Sherwin Industries, Inc."></a>
+	    <a href="<?php echo home_url(); ?>" id="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-old.png" alt="Sherwin Industries, Inc."></a>
       
       <input type="checkbox" id="toggle-menu" role="button">
       <label for="toggle-menu"></label>
