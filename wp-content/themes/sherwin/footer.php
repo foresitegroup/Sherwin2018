fc11dc2a4e7d792ebebb8ff00fc6d567f371bf43
foresitegroup <mark@foresitegrp.com>
2018-12-12 09:43:03 -0600

    Reattach MailChimp form to list

--- wp-content/themes/sherwin/footer.php
+++ wp-content/themes/sherwin/footer.php
@@ -26,14 +26,14 @@
   <!-- Begin Mailchimp Signup Form -->
   <div id="mc_embed_signup">
     Receive periodic <span>news</span> and <span>product information</span>
-    <form action="https://sherwinindustriesinc.us19.list-manage.com/subscribe/post?u=6567fb15ec7bd620806521ea0&amp;id=d0b81e5dbc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate site-width" target="_blank" novalidate>
+    <form action="https://sherwinindustriesinc.us19.list-manage.com/subscribe/post?u=6567fb15ec7bd620806521ea0&amp;id=7d3a271abb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate site-width" target="_blank" novalidate>
       <div id="mc_embed_signup_scroll">
         <div id="mce-responses" class="clear">
           <div class="response" id="mce-error-response" style="display:none"></div>
           <div class="response" id="mce-success-response" style="display:none"></div>
         </div>
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6567fb15ec7bd620806521ea0_d0b81e5dbc" tabindex="-1" value=""></div>
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6567fb15ec7bd620806521ea0_7d3a271abb" tabindex="-1" value=""></div>
 
         <input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="Email">
         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
